6c08fd6207a0ca5c28fa078c9a6fd79ce080b7ec
Albin05 <97468317+Albin05@users.noreply.github.com>
2022-03-21 10:38:27 +0530

    Evaluation

---
+++ src/model/comment.model.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const commentSchema = new mongoose.Schema(
+    {
+        body: {type: String, required: true},
+        userId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "user",
+            required: true,
+        },
+        bookId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "book",
+            required: true,
+        }
+    },
+    {
+        versionKey: false,
+        timestamps: true,
+    }
+)
